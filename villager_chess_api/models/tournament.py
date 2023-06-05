fbfdd22a7d336a7844ac12288b0dfc464b3e6d29
Mary Williams <sbyardnss@gmail.com>
2023-06-05 15:10:47 -0500

    working on tournament functionality. added competitor fields to fixture and model before switching branches. delete those if there is an issue

--- villager_chess_api/models/tournament.py
+++ villager_chess_api/models/tournament.py
@@ -6,6 +6,7 @@ class Tournament(models.Model):
     title = models.CharField(max_length=50)
     creator = models.ForeignKey('Player', on_delete=models.SET_DEFAULT, default=1, related_name="my_tournaments")
     date = models.DateField()
+    competitors = models.ManyToManyField('Player', related_name='joined_tournaments')
     time_setting = models.ForeignKey(
         'TimeSetting', on_delete=models.DO_NOTHING, default=0,
         related_name="tournaments_with_time_setting")
