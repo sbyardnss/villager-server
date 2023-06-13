a1fce025dd623a3e689d945a94559b2f5ea3065b
Mary Williams <sbyardnss@gmail.com>
2023-06-13 15:17:58 -0500

    ai responses seem to be working

--- villager_chess_api/views/ai_handler.py
+++ villager_chess_api/views/ai_handler.py
@@ -14,23 +14,32 @@ import openai
 api_key = os.getenv("CHESS_KEY")
 openai.api_key = api_key
 
-
+# class AiSerializer(serializers.Serializer):
+#     class Meta:
+#         fields = ('move')
 class AiView(ViewSet):
     """coordinates responses to ai requests"""
 
     def create(self, request):
         # if self.path == "/ai":
-
-            # self._set_headers(201)
-
+        fen = request.data['fen']
+        pgn = request.data['pgn']
+        possible_moves = request.data['possibleMoves']
+        color = request.data['color']
+        # self._set_headers(201)
 
         instructions = [
-            {"role": "system", "content": "you are a professional chess player. Based on the pgn provided what should the next move be? Respond only in the 'short algebraic notation' format. Nothing more."},
-            {"role": "user", "content": f"{request.data['pgn']}"}
+            {"role": "system", "content": "You are a helpful assistant,"},
+            # {"role": "user", "content": f"You are playing chess against the user as {color} and trying to win the game. The current PGN is {pgn}. The current FEN notation is {fen}. The valid moves available to you are {possible_moves}. Pick a move from the list that maximizes your chance of winning and provide an explanation for why you chose that option. Return your response in JSON object format with these properties: 'move' and 'explanation'. Pick a move even if you don't have a good option."}
+            {"role": "user", "content": f"You are playing chess against the user as {color} and trying to win the game. The current PGN is {pgn}. The current FEN notation is {fen}. The valid moves available to you are {possible_moves}. Pick a move from the list that maximizes your chance of winning. Return your response in the same format as this example: 'move: e5'. Pick a move even if you don't have a good option and do not provide any data outside of the provided format."}
+
         ]
+
         move_completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", messages=instructions)
+
         response = move_completion.choices[0].message.content
+        # serialized = AiSerializer(response)
         return Response(response, status=status.HTTP_201_CREATED)
 
 #     def _set_headers(self, status):
@@ -54,7 +63,7 @@ class AiView(ViewSet):
 #                          'GET, POST, PUT, DELETE')
 #         self.send_header('Access-Control-Allow-Headers',
 #                          'X-Requested-With, Content-Type, Accept')
-        
+
 #         self.end_headers()
 
 # def main():
