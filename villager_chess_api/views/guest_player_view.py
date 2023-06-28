59199ce20d9c5e7a8e97dda21eff65e735e27975
Mary Williams <sbyardnss@gmail.com>
2023-06-28 18:12:05 -0500

    added create and destroy for guests

--- villager_chess_api/views/guest_player_view.py
+++ villager_chess_api/views/guest_player_view.py
@@ -8,6 +8,9 @@ class GuestPlayerSerializer(serializers.ModelSerializer):
         model = GuestPlayer
         fields = ('id', 'name', 'guest_id')
 
+class CreateGuestPlayerSerializer(serializers.ModelSerializer):
+    class Meta: 
+        fields = ['id', 'name']
 class GuestView(ViewSet):
     """handles rest requests for guest users for tournament participation"""
     def list(self, pk=None):
@@ -19,3 +22,11 @@ class GuestView(ViewSet):
         guest = GuestPlayer.objects.get(guest_id = request.data['guestId'])
         serialized = GuestPlayerSerializer(guest, many=False)
         return Response(serialized.data, status=status.HTTP_200_OK)
+    def create(self, request):
+        serialized = CreateGuestPlayerSerializer(data=request.data)
+        serialized.is_valid(raise_exception=True)
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+    def destroy(self, request, pk=None):
+        guest = GuestPlayer.objects.get(pk=pk)
+        guest.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
