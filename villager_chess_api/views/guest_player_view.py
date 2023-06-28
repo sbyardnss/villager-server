dec9c180b1a6d6afc889fab77b90d5c10cb82470
Mary Williams <sbyardnss@gmail.com>
2023-06-28 18:47:01 -0500

    updated

--- villager_chess_api/views/guest_player_view.py
+++ villager_chess_api/views/guest_player_view.py
@@ -13,15 +13,13 @@ class CreateGuestPlayerSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 class GuestView(ViewSet):
     """handles rest requests for guest users for tournament participation"""
-    # def list(self, pk=None):
-    #     guests = GuestPlayer.objects.all()
-    #     serialized = GuestPlayerSerializer(guests, many=True)
-    #     return Response(serialized.data, status=status.HTTP_200_OK)
-
+    def list(self, pk=None):
+        guests = GuestPlayer.objects.all()
+        serialized = GuestPlayerSerializer(guests, many=True)
+        return Response(serialized.data, status=status.HTTP_200_OK)
     def retrieve(self, request, pk=None):
         guest_id = pk
         numeric_id = guest_id.split("g")[1]
-        print(numeric_id)
         guest = GuestPlayer.objects.get(pk = numeric_id)
         serialized = GuestPlayerSerializer(guest, many=False)
         return Response(serialized.data, status=status.HTTP_200_OK)
