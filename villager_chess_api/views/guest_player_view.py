b39551b25a106bd409eeef2b77e94d3f839f7d4c
Mary Williams <sbyardnss@gmail.com>
2023-06-28 18:45:50 -0500

    built retrieve for guest ids that works

--- villager_chess_api/views/guest_player_view.py
+++ villager_chess_api/views/guest_player_view.py
@@ -13,13 +13,16 @@ class CreateGuestPlayerSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 class GuestView(ViewSet):
     """handles rest requests for guest users for tournament participation"""
-    def list(self, pk=None):
-        guests = GuestPlayer.objects.all()
-        serialized = GuestPlayerSerializer(guests, many=True)
-        return Response(serialized.data, status=status.HTTP_200_OK)
+    # def list(self, pk=None):
+    #     guests = GuestPlayer.objects.all()
+    #     serialized = GuestPlayerSerializer(guests, many=True)
+    #     return Response(serialized.data, status=status.HTTP_200_OK)
 
-    def retrieve(self, request):
-        guest = GuestPlayer.objects.get(guest_id = request.data['guestId'])
+    def retrieve(self, request, pk=None):
+        guest_id = pk
+        numeric_id = guest_id.split("g")[1]
+        print(numeric_id)
+        guest = GuestPlayer.objects.get(pk = numeric_id)
         serialized = GuestPlayerSerializer(guest, many=False)
         return Response(serialized.data, status=status.HTTP_200_OK)
     def create(self, request):
@@ -27,6 +30,6 @@ class GuestView(ViewSet):
         serialized.is_valid(raise_exception=True)
         return Response(serialized.data, status=status.HTTP_201_CREATED)
     def destroy(self, request, pk=None):
-        guest = GuestPlayer.objects.get(pk=pk)
+        guest = GuestPlayer.objects.get(guest_id = request.data['guestId'])
         guest.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
