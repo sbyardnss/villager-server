af75d9d463c2443807b04b3413cf4cabbc188b6f
Mary Williams <sbyardnss@gmail.com>
2023-07-07 10:56:30 -0500

    updated

--- villager.sql
+++ villager.sql
@@ -9,7 +9,7 @@ DELETE FROM villager_chess_api_tournament
 WHERE id > 1
 
 DELETE FROM villager_chess_api_game
-WHERE id >481
+WHERE id >488
 
 UPDATE villager_chess_api_tournament
 SET complete = 0
@@ -32,8 +32,8 @@ DELETE FROM villager_chess_api_player_friends where id > 13
 UPDATE villager_chess_api_game
 -- SET PGN = '1. d4 a5 2. Bf4 Nf6 3. e3 e6 4. Nc3 a4 5. Nb5 Ba3 6. Nxc7+ Ke7 7. Nxa8 Ng4 8. Qxg4 g6 9. bxa3 b6 10. Bb5 h6 11. Bg5+ Ke8 12. Bxd8 Ba6 13. Bxb6 f6 14. Bxa6 Rh7 15. Qxg6+ Ke7 16. Qxh7+ Ke8 17. Bc5 f5'
 -- SET win_style = '' 
-SET winner_id = null, PGN = '1. d4 a5 2. Bf4 Nf6 3. e3 e6 4. Nc3 a4 5. Nb5 Ba3 6. Nxc7+ Ke7 7. Nxa8 Ng4 8. Qxg4 g6 9. bxa3 b6 10. Bb5 h6 11. Bg5+ Ke8 12. Bxd8 Ba6 13. Bxb6 f6 14. Bxa6 Rh7 15. Qxg6+ Ke7 16. Qxh7+ Ke8 17. Bc5 f5', win_style = '' 
-WHERE ID = 426
+SET PGN = '1. d4 a5 2. Bf4 Nf6 3. e3 e6 4. Nc3 a4 5. Nb5 Ba3 6. Nxc7+ Ke7 7. Nxa8 Ng4 8. Qxg4 g6 9. bxa3 b6 10. Bb5 h6 11. Bg5+ Ke8 12. Bxd8 Ba6 13. Bxb6 f6 14. Bxa6 Rh7 15. Qxg6+ Ke7 16. Qxh7+ Ke8 17. Bc5 f5', win_style = '' 
+WHERE ID = 478
 
 UPDATE villager_chess_api_game
 SET win_style = ''
