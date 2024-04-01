d0d50c3802dd2791f505f6318f6ea649ab0245a2
Mary Williams <sbyardnss@gmail.com>
2024-04-01 18:18:28 -0500

    docker db available when using docker container

---
+++ docker.sql
@@ -0,0 +1,2 @@
+DELETE FROM villager_chess_api_game
+WHERE ID = 1646
--- villager_chess/settings.py
+++ villager_chess/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import django_on_heroku
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-xw8gkue0a=_wma%rcc*$)i-3%(h71!lj&mpuc1c0#8&rgkv$l_'
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -123,13 +126,38 @@ WSGI_APPLICATION = 'villager_chess.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
+if os.getenv('CONTAINER')=='local':
+  DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+if os.getenv('CONTAINER')=='docker':
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'docker.sqlite3',
+    }
+# Override with PostgreSQL settings if environment variables are set
+# NEED TO ADDRESS USAGE OF GUEST USER BEFORE ABLE TO CREATE DATABASE FROM SCRATCH IN DOCKER BUILD
+# NEED TO PIP INSTALL PYTHON-DECOUPLE IF YOU GO THIS ROUTE
+# if os.environ.get('DATABASE_ENGINE'):
+#     DATABASES['default'] = {
+#         'ENGINE': config('DATABASE_ENGINE'),
+#         'NAME': config('DATABASE_NAME'),
+#         'USER': config('DATABASE_USER'),
+#         'PASSWORD': config('DATABASE_PASSWORD'),
+#         'HOST': config('DATABASE_HOST'),
+#         'PORT': config('DATABASE_PORT'),
+#     }
+# if os.environ.get('DATABASE_ENGINE'):
+#     print('using docker db')
+#     DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'docker.sqlite3',
+#     }
+
+
 
 
 # Password validation
