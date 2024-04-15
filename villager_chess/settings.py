6d24b72f46ad2c2d8175eb65a76a0129f3ba6066
Stephen Byard <sbyardnss@gmail.com>
2024-04-15 10:30:33 -0500

    ready for v2 deploy

--- villager_chess/settings.py
+++ villager_chess/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-# from dotenv import load_dotenv GOOGLE THIS. IT CAN BE USED TO ADD ENVIRONMENT VARIABLES TO THE SETTINGS.PY
+
 from pathlib import Path
 import os
 import django_on_heroku
@@ -24,11 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xw8gkue0a=_wma%rcc*$)i-3%(h71!lj&mpuc1c0#8&rgkv$l_'
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
